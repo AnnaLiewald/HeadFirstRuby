702d4a9826f047641f20f63d5b5860c3ebadb1ee
AnnaLiewald <anna.liewald@studitemps.de>
2017-12-13 00:27:33 +0100

    adds validation to Cat Class

--- animals.rb
+++ animals.rb
@@ -1,80 +1,103 @@
 class Bird
+
+attr_reader :name, :age
+
   def make_up_name
     @name = "Tweety"
   end
-  def talk
-    puts "#{@name} says 'Chirp! Chirp!''"
+
+  def make_up_age
+    @age = 5
   end
+
+  def report_age
+    puts "#{@name} is #{@age} years old."
+  end
+
+  def talk
+    puts "#{@name} says 'Chirp, chirp'!"
+  end
+
   def move(destination)
-    puts "#{@name} flies to the #{destination}."
+    puts "#{@name} runs to the #{destination}."
   end
 end
 
 
-
+# The class Cat even has validations!
 class Cat
-# Write a new value to @name
 
-attr_accessor :name, :age
+  attr_reader :name, :age
 
-#  def name=(new_value)
-#    @name = new_value
-#  end
-# Read the value from @name
-#  def name
-#    @name
-#  end
-#  def age= (new_value)
-#    @age = new_value
-#  end
-  #def age
-  #  @age
-  #end
+  def name=(value)
+    if @name == ""
+      raise "Name can't be blank."
+    end
+    @name = value
+  end
+
+  def age=(value)
+   if value < 0
+     raise "An age of #{value} isn't valid!"
+   end
+   @age = value
+  end
 
   def report_age
-    puts "#{@name} is #{@age} years old."
+    if @age > 1
+      puts "#{@name} is #{@age} years old."
+    else
+      puts  "#{@name} is #{@age} year old."
+    end
   end
 
   def talk
     puts "#{@name} says 'Meow'!"
   end
+
   def move(destination)
     puts "#{@name} sneaks to the #{destination}."
   end
+
 end
 
 
 class Dog
-  def make_up_name
-    @name = "Rex"
-  end
-  def talk
-    puts "#{@name} says 'Bark'!"
-  end
-  def move(destination)
-    puts "#{@name} runs to the #{destination}."
-  end
+
+  attr_accessor :age, :name
+
   def make_up_age
     @age = 5
   end
+
   def report_age
     puts "#{@name} is #{@age} years old."
   end
+
+  def talk
+    puts "#{@name} says 'Bark'!"
+  end
+
+  def move(destination)
+    puts "#{@name} runs to the #{destination}."
+  end
+
 end
 
 
+cat = Cat.new
+cat.name = "Fluffy"
+cat.age = 1
+cat.report_age
+cat.talk
+cat.move ('litter box')
+
 dog = Dog.new
-dog.make_up_name
+dog.name = "Rex"
 dog.make_up_age
 dog.report_age
 dog.talk
 dog.move('fence')
-
-cat = Cat.new
-cat.name = "Fluffy"
-cat.age = 2
-cat.talk
-cat.move ('litter box')
 
 bird = Bird.new
 bird.make_up_name
